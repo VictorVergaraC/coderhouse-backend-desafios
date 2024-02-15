75ef2e20bca479c788959d7ed5fb13d6c1374ead
vvergara <victorvergara.c99@gmail.com>
2024-02-15 11:05:33 -0300

    Correciones de export en los manager.js

--- src/models/CartManager.js
+++ src/models/CartManager.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
+const __dirname = dirname(__filename);
 
 export class CartManager {
     constructor() {
